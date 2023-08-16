fa8673fcbc561013f1c5687325970e86bcbfd5bc
K-Kelvin <kelvinkijanda@gmail.com>
2023-08-16 13:58:23 +0300

    Added settings.py

--- jet_game.py
+++ jet_game.py
@@ -1,17 +1,12 @@
 import pygame, random, time, os.path
 from pygame.locals import *
+from .settings import (
+    BLACK, SKYBLUE, WHITE, RED, GRAY, GREEN, MAGENTA,
+)
+
 pygame.mixer.init()
 pygame.init()
 
-# setting up colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-GRAY = (127, 127, 127)
-MAGENTA = (200, 0 ,200)
-SKYBLUE = (135, 206, 250)
 
 # setting up the  screen
 flag = RESIZABLE
---
+++ settings.py
@@ -0,0 +1,9 @@
+# setting up colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (127, 127, 127)
+MAGENTA = (200, 0 ,200)
+SKYBLUE = (135, 206, 250)
